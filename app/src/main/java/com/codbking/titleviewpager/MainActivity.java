8869e974f3f524ec85f7fcfbe84739602ade5b78
codbking <codbking@gmail.com>
2016-12-13 21:54:42 +0800

    add lib

--- app/src/main/java/com/codbking/titleviewpager/MainActivity.java
+++ app/src/main/java/com/codbking/titleviewpager/MainActivity.java
@@ -3,10 +3,13 @@ package com.codbking.titleviewpager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.codbking.titleviewpager.view.TitleViewPager;
+
+import com.codbking.library.view.TitleViewPager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
